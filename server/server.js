8346c0ae3321aebaedf25746c13de8ef1d5d170c
Alexander <96749659+Nazeofel@users.noreply.github.com>
2024-10-16 14:45:52 +0200

    dead code remove

--- server/server.js
+++ server/server.js
@@ -1,25 +1,15 @@
-// proxy.js
 import express from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
-import cors from "cors"; // Optional, for CORS handling
+import cors from "cors";
 import { join } from "path";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 const app = express();
-const PORT = 3000; // Proxy server port
+const PORT = 3000;
 
 app.use(express.static(join(process.cwd(), "dist")));
 
-app.use(cors()); // Enable CORS if your React Native app makes requests from a different origin
-app.use(express.json()); // Middleware to parse JSON bodies
-// app.use((req, res, next) => {
-//   res.removeHeader("Content-Security-Policy");
-//   //   if (req.query.platform) {
-//   //     req.query.platform = "web";
-//   //   }
-//   next(); // Pass the request to the next middleware or route handler
-// });
-
+app.use(cors());
+app.use(express.json());
 app.post("/api/token", async (req, res) => {
   const { code } = await req.body;
 
@@ -42,17 +32,6 @@ app.post("/api/token", async (req, res) => {
 app.get("*", (req, res) => {
   res.sendFile(join(process.cwd(), "dist", "index.html"));
 });
-// app.use(
-//   "/", // Adjust the path if you want to forward specific routes
-//   createProxyMiddleware({
-//     target: "http://localhost:3000", // Main server
-//     changeOrigin: true,
-//     pathRewrite: {
-//       "/": "", // Remove '/api' from the request URL if necessary
-//     },
-//   })
-// );
-
 app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
 });
